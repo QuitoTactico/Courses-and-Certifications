e339d14678267b725d371cfcb6a66e52a935115b
QuitoTactico <esteban.vergara@globant.com>
2025-02-19 18:59:53 -0500

    reformatted with black

--- Udemy 2 - REST APIs with Flask and Python in 2025/5 - Flask App Efficiency Updates/resources/item.py
+++ Udemy 2 - REST APIs with Flask and Python in 2025/5 - Flask App Efficiency Updates/resources/item.py
@@ -6,7 +6,8 @@ from flask_smorest import Blueprint, abort
 from db import items
 from schemas import ItemSchema, ItemUpdateSchema
 
-blp = Blueprint("items", __name__, description = "Operations on items")
+blp = Blueprint("items", __name__, description="Operations on items")
+
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
@@ -15,25 +16,26 @@ class Item(MethodView):
         try:
             return items[item_id]
         except KeyError:
-            abort(404, message='Item not found')
+            abort(404, message="Item not found")
 
     def delete(self, item_id):
         try:
             del items[item_id]
-            return {'message': 'Item deleted'}
+            return {"message": "Item deleted"}
         except KeyError:
-            abort(404, message='Item not found')
+            abort(404, message="Item not found")
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_id, item_data):
-        #item_data = request.get_json()
+        # item_data = request.get_json()
         try:
             item = items[item_id]
-            item |= item_data       # que buena forma!
+            item |= item_data  # que buena forma!
             return item
         except KeyError:
-            abort(404, message='Item not found')
+            abort(404, message="Item not found")
+
 
 @blp.route("/item")
 class ItemList(MethodView):
@@ -41,9 +43,9 @@ class ItemList(MethodView):
     def get(self):
         # por culpa del uso de marshmallow, ya no es un objeto/diccionario con la lista de items, sino meramente la lista y ya
         # recuerda que era mejor el objeto/diccionario, por si el día de mañana te daba por enviar cosas extras
-        #return {'items': list(items.values())}
+        # return {'items': list(items.values())}
         return items.values()
-    
+
     @blp.arguments(ItemSchema)
     @blp.response(200, ItemSchema)
     def post(self, item_data):
@@ -52,16 +54,13 @@ class ItemList(MethodView):
 
         for item in items.values():
             if (
-                item_data['name'] == item['name']
-                and item_data['store_id'] == item['store_id']
+                item_data["name"] == item["name"]
+                and item_data["store_id"] == item["store_id"]
             ):
-                abort(
-                    400,
-                    message='Item already exists'
-                )
+                abort(400, message="Item already exists")
 
         item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id} # pasamos todas las weas del dict
+        item = {**item_data, "id": item_id}  # pasamos todas las weas del dict
         items[item_id] = item
 
         return item, 201
--- Udemy 2 - REST APIs with Flask and Python in 2025/5 - Flask App Efficiency Updates/resources/store.py
+++ Udemy 2 - REST APIs with Flask and Python in 2025/5 - Flask App Efficiency Updates/resources/store.py
@@ -6,7 +6,8 @@ from flask_smorest import Blueprint, abort
 from db import stores
 from schemas import StoreSchema
 
-blp = Blueprint("stores", __name__, description = "Operations on stores")
+blp = Blueprint("stores", __name__, description="Operations on stores")
+
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
@@ -15,37 +16,32 @@ class Store(MethodView):
         try:
             return stores[store_id]
         except KeyError:
-            abort(404, message='Store not found')
+            abort(404, message="Store not found")
 
     def delete(self, store_id):
         try:
             del stores[store_id]
-            return {'message': 'Store deleted'}
+            return {"message": "Store deleted"}
         except KeyError:
-            abort(404, message='Store not found')
+            abort(404, message="Store not found")
 
 
 @blp.route("/store")
 class StoreList(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        #return {'stores': list(stores.values())}
+        # return {'stores': list(stores.values())}
         # lo de arriba ya no.
         return stores
-    
+
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema)
     def post(self, store_data):
         for store in stores.values():
-            if (
-                store_data['name'] == store['name']
-            ):
-                abort(
-                    400,
-                    message='Store already exists'
-                )
+            if store_data["name"] == store["name"]:
+                abort(400, message="Store already exists")
 
         store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id} # pasamos todas las weas del dict
+        store = {**store_data, "id": store_id}  # pasamos todas las weas del dict
         stores[store_id] = store
         return store

3e526defb3d6a13c7a1bb4f7d624569ce069106e
QuitoTactico <esteban.vergara@globant.com>
2025-03-03 12:44:20 -0500

    spoiler: it didn't work

---
+++ 04 - Udacity - SQL for Data Analysis/markdown-to-slack.py
@@ -0,0 +1,26 @@
+# Well, it gives the slack format, but now what XD, it still can't recognize code blocks
+
+# Credits: https://stackoverflow.com/questions/41271577/is-there-a-way-to-upload-a-markdown-file-as-a-slack-post
+
+import re
+import sys
+
+REGEX_REPLACE = (
+  (re.compile('^- ', flags=re.M), '• '),
+  (re.compile('^  - ', flags=re.M), '  ◦ '),
+  (re.compile('^    - ', flags=re.M), '    ⬩ '),
+  (re.compile('^      - ', flags=re.M), '    ◽ '),
+  (re.compile('^#+ (.+)$', flags=re.M), r'*\1*'),
+  (re.compile('\*\*'), '*'),
+)
+
+def main(i, o):
+  s = i.read()
+  for regex, replacement in REGEX_REPLACE:
+    s = regex.sub(replacement, s)
+  o.write(s)
+
+if __name__ == '__main__':
+  with open(sys.argv[1], encoding='utf-8') as i, \
+       open(sys.argv[1] + '.slack', 'w', encoding='utf-8') as o:
+    main(i, o)
